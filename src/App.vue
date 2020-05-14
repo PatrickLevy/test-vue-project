28f775b248a8015f385b46ac3ec47396a01ed81c
Patrick Levy <patricklevy@exosite.com>
2020-05-14 13:55:38 -0500

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App yay. I updated it again 2."/>
+    <HelloWorld msg="Welcome to Your Vue.js App yay. This is a test pr."/>
   </div>
 </template>
 
