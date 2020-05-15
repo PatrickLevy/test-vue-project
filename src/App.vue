a3b15b04537ff0a713bf81d7015b527cb4cc0d27
Patrick Levy <patricklevy@exosite.com>
2020-05-15 08:28:08 -0500

    Update to test caching

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App yay. The build is served and updated from s3."/>
+    <HelloWorld msg="Welcome to Your Vue.js App yay. The build is served and updated from s3. Here is an update to test caching."/>
   </div>
 </template>
 
