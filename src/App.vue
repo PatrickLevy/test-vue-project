0119dad2412de0a1a941e50cb9d0d29987ee6d72
Patrick Levy <patricklevy@exosite.com>
2020-05-15 08:16:50 -0500

    another test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App yay. This is a test pr that should be connected to travis.org. Let's see if this worked."/>
+    <HelloWorld msg="Welcome to Your Vue.js App yay. The build is served and updated from s3."/>
   </div>
 </template>
 
